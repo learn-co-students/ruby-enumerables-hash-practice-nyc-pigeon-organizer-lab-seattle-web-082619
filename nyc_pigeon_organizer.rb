8dcbb9b509fbaa02c890405cd6379f37808556d9
Justin Maple <justinamaple@gmail.com>
2019-08-21 20:55:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,7 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
 
-  data.each do |trait|
-    key = trait[0]
-    values = trait[1]
-    
+  data.each_pair do |key, values|
     values.each_pair do |value, birds|
       birds.each do |bird|
         if(!pigeon_list[bird])
@@ -15,7 +12,6 @@ def nyc_pigeon_organizer(data)
         end
         
         pigeon_list[bird][key].push(value.to_s)
-        
       end
     end
   end
